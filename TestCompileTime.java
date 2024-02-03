1aa4862e808bedfadcf010ae63c016e8c4e8b183
LUC4SWM <lawoldemichael@gmail.com>
2024-02-03 15:36:00 -0800

    Done A3

--- TestCompileTime.java
+++ TestCompileTime.java
@@ -6,27 +6,27 @@ public class TestCompileTime
 
 	public static void main(String[] args)
 	{
+		int[] numbers = new int[4];
+		for (int i=0; i<4; i++)
+		{
+			numbers[i] = i;
+		}
 		if(Debug.DEBUG)
   		{
 			System.err.println("Debug is turned on");
+			System.out.println();
 			TestCompileTime t = new TestCompileTime();
-	        int[] numbers = new int[4];
-	        for (int i=1; i<5; i++)
-	        {
-	            numbers[i] = i;
-	            System.out.println("DEBUG_STATEMENT: number["+i+"]="+i);
-
-	        }
-
-	        for (int i=1; i<5; i++)
-	        {
-	            int element = numbers[i];
-	            System.out.print(element + " ");
-	        }
-	        System.out.println();
+	        for (int i : numbers){
+				System.out.println("DEBUG_STATEMENT: number[" + i + "]=" + i);
+			}
   		}
-  		else
-  			System.out.println("Debug is turned off");
+  		else {
+			System.out.println("Debug is turned off");
+		}
+		for (int i=0; i<4; i++){
+			int element = numbers[i];
+			System.out.print(element + ", ");
+		}
  	}
 
 
